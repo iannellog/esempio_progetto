60ce0e43e963ee520ab23280f56ed4e077f14ff0
Giulio Iannello <g.iannello@unicampus.it>
2022-09-02 09:48:49 +0200

    Primo commit: bozza di programma principale

---
+++ Tombola.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  1 10:29:04 2022
+
+@author: iannello
+"""
+
+# Acquisisci quanti giocatori e quante cartelle per giocatore
+# Crea tabellone
+# Genera un numero sufficiente di cartelle a gruppi di 6
+# Distribuisci le cartelle ai giocatori
+# Ripeti
+#   estrai un numero
+#   aggiorna tabellone e tabelle
+#   rileva vincite
+#   se qualcuno ha fatto tombola termina il gioco
