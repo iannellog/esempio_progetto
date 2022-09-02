42fd32156d6e52250973167f38d372977a3cf276
Giulio Iannello <g.iannello@unicampus.it>
2022-09-02 10:31:32 +0200

    Verifica che i dati da linea di comando siano coerenti e segnalazione nel caso non lo siano

--- Tombola.py
+++ Tombola.py
@@ -8,6 +8,8 @@ Created on Thu Sep  1 10:29:04 2022
 Sep 2,2022:
 - Acquisizione da riga di comando del numero di giocatori e
 del numero di cartelle per giocatore
+- Verifica che i dati da linea di comando siano coerenti e
+segnalazione nel caso non lo siano
 """
 
 from argparse import ArgumentParser
@@ -26,7 +28,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
     n_giocatori = args.n_giocatori
     n_cartelle = args.n_cartelle
-    print(f'Vi sono {n_giocatori} giocatori che chiedono rispettivamente {n_cartelle} cartelle')
+    if len(n_cartelle) == n_giocatori:
+        print(f'Vi sono {n_giocatori} giocatori che chiedono rispettivamente {n_cartelle} cartelle')
+    else:
+        print(f'Il numero di giocatori non corrisponde alla lunghezza della lista di cartelle da assegnare')
+        exit(1)
 
     # Crea tabellone
     # Genera un numero sufficiente di cartelle a gruppi di 6
